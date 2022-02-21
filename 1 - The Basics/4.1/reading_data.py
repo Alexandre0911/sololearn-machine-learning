32090187a92cbe10c5b1e899237e2a8d8a224a33
Alexandre <71990615+Alexandre0911@users.noreply.github.com>
2022-02-21 10:52:11 +0000

    Finished Reading Data exercise

---
+++ 1 - The Basics/4.1/reading_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+pd.options.display.max_columns = 6         #? Option to make pandas display a max number of 6 columns
+
+
+
+df = pd.read_csv('C:\\Users\\vampi\\Documents\\GitHub\\sololearn-machine-learning\\1 - The Basics\\4.1\\titanic.csv')           #! The "df" object is a DataFrame which contains a Titanic DataSet
+
+
+
+print(df.head())            #! head() function returns first 5 rows of a DataFrame
+print(df.describe())            #! describe() function returns a table of statistics about the columns of our DataFrame
+
+
+
+col = df['Fare']          #* To select a single column, we use the square brackets and the column name.
+print(col)
+
+
+#! You should uncomment or comment whatever you want or not to be run!
